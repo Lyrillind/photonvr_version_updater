761f3027a9f185af7925c911b8a4f0e291c0e80a
Murriouz <Murriouz@gmail.com>
2017-01-25 13:27:59 +0800

    更新到 0.9.10b

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ var appId = 'VPVzwjElxgAwTwJBbdEwoMlx-gzGzoHsz';
 var appKey = 'h9XawPpBQqLHEMj7eDFfVIU5';
 _leancloudStorage2.default.init({ appId: appId, appKey: appKey });
 
-var versionNumber = '0.9.1b';
+var versionNumber = '0.9.10b';
 
 var Version = _leancloudStorage2.default.Object.extend('EditorVersions');
 var newVersion = new Version();
@@ -21,10 +21,10 @@ newVersion.set('linkWin64', 'http://7xlt5v.com1.z0.glb.clouddn.com/Photon%20Engi
 newVersion.set('linkBaiduMacOS', 'https://pan.baidu.com/s/1hsuTbxU');
 newVersion.set('linkBaiduWin32', 'https://pan.baidu.com/s/1c21gNFa');
 newVersion.set('linkBaiduWin64', 'https://pan.baidu.com/s/1pL10tVP');
-newVersion.set('macSize', '388.27 MB');
+newVersion.set('macSize', '388.28 MB');
 newVersion.set('win32Size', '296.14 MB');
 newVersion.set('win64Size', '309.18 MB');
-newVersion.set('releaseNote', '紧急更新：修正了在关闭编辑器时可能导致项目文件损坏的问题。同时增加了项目文件自动备份的功能。建议所有用户更新。');
+newVersion.set('releaseNote', '');
 
 newVersion.save().then(function (ver) {
   console.log(ver.id + ' saved!');
