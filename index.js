16e75279018825e4d72192c728d14180fde8ee08
Murriouz <Murriouz@gmail.com>
2017-02-04 19:27:36 +0800

    更新版本号到 0.9.11b

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ var appId = 'VPVzwjElxgAwTwJBbdEwoMlx-gzGzoHsz';
 var appKey = 'h9XawPpBQqLHEMj7eDFfVIU5';
 _leancloudStorage2.default.init({ appId: appId, appKey: appKey });
 
-var versionNumber = '0.9.10b';
+var versionNumber = '0.9.11b';
 
 var Version = _leancloudStorage2.default.Object.extend('EditorVersions');
 var newVersion = new Version();
@@ -18,13 +18,13 @@ newVersion.set('version', versionNumber);
 newVersion.set('linkMacOS', 'http://7xlt5v.com1.z0.glb.clouddn.com/Photon%20Engine%20' + versionNumber + '.pkg');
 newVersion.set('linkWin32', 'http://7xlt5v.com1.z0.glb.clouddn.com/Photon%20Engine%20Setup%20' + versionNumber + '-ia32.exe');
 newVersion.set('linkWin64', 'http://7xlt5v.com1.z0.glb.clouddn.com/Photon%20Engine%20Setup%20' + versionNumber + '-x64.exe');
-newVersion.set('linkBaiduMacOS', 'https://pan.baidu.com/s/1i56wYwt');
-newVersion.set('linkBaiduWin32', 'https://pan.baidu.com/s/1bpy1Kj9');
-newVersion.set('linkBaiduWin64', 'https://pan.baidu.com/s/1gfmOtk3');
+newVersion.set('linkBaiduMacOS', 'https://pan.baidu.com/s/1kVJkBB5');
+newVersion.set('linkBaiduWin32', 'https://pan.baidu.com/s/1kVkEIz1');
+newVersion.set('linkBaiduWin64', 'https://pan.baidu.com/s/1c2xemjA');
 newVersion.set('macSize', '388.28 MB');
-newVersion.set('win32Size', '296.14 MB');
-newVersion.set('win64Size', '309.18 MB');
-newVersion.set('releaseNote', '');
+newVersion.set('win32Size', '296.15 MB');
+newVersion.set('win64Size', '309.17 MB');
+newVersion.set('releaseNote', '紧急修复windows上包含全景图片的工程编译失败的问题');
 
 newVersion.save().then(function (ver) {
   console.log(ver.id + ' saved!');
