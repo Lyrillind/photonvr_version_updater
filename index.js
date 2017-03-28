86bd027aaf4a6248dff44a7fe3bbbc3ac658daae
Murriouz <Murriouz@gmail.com>
2017-03-29 00:32:34 +0800

    0.9.30b

--- index.js
+++ index.js
@@ -17,19 +17,21 @@ var onlineServer = {
 
 _leancloudStorage2.default.init(devServer);
 
+var versionNumber = '0.9.30b';
+
 var Version = _leancloudStorage2.default.Object.extend('EditorVersions');
 var newVersion = new Version();
 newVersion.set('version', versionNumber);
 newVersion.set('linkMacOS', 'http://7xlt5v.com1.z0.glb.clouddn.com/Photon%20Engine%20' + versionNumber + '.pkg');
 newVersion.set('linkWin32', 'http://7xlt5v.com1.z0.glb.clouddn.com/Photon%20Engine%20Setup%20' + versionNumber + '-ia32.exe');
 newVersion.set('linkWin64', 'http://7xlt5v.com1.z0.glb.clouddn.com/Photon%20Engine%20Setup%20' + versionNumber + '-x64.exe');
-newVersion.set('linkBaiduMacOS', 'https://pan.baidu.com/s/1geVoAxt');
-newVersion.set('linkBaiduWin32', 'https://pan.baidu.com/s/1hrS4PnQ');
-newVersion.set('linkBaiduWin64', 'https://pan.baidu.com/s/1bpiogpT');
-newVersion.set('macSize', '390.24 MB');
-newVersion.set('win32Size', '298.09 MB');
-newVersion.set('win64Size', '311.10 MB');
-newVersion.set('releaseNote', '原有功能的调整\n分离了视频、音频、图片节点的“淡入淡出”，现在可以单独勾选淡入和淡出，并且按需要分别设置时长。\n\n新增功能\n1. 动态音量调节。\n	音频节点支持添加任意数量的音量调节点，在运行时动态的进行调整。典型案例是听到别人心里的声音。场景里有三个人，观众目光看向谁就能听到谁的心声。作品中其实共有三条音轨同时播放，根据观众的目光去调节其他两条音轨的音量就可以实现。这个技术还能运用在动态为音乐加入不同的乐器，或者在同一个节奏下转换乐曲的情绪。\n\n2. 累加触发节点。\n	支持设置累加计算，当累加的值到达特定数字的时候触发。\n	节点的设计初衷是为了让观众在当前场景的一系列动作影响到后续播放哪一个新的场景。比如，场景是一场面试，过程中如果观众总在东张西望，那么面试结束后就播放“面试失败”分支，否则播放“面试成功”分支。这个节点的引入可以实现非常复杂的网状剧情结构。');
+newVersion.set('linkBaiduMacOS', 'https://pan.baidu.com/s/1hsMb78K');
+newVersion.set('linkBaiduWin32', 'https://pan.baidu.com/s/1gfonAxl');
+newVersion.set('linkBaiduWin64', 'https://pan.baidu.com/s/1i4Phjnv');
+newVersion.set('macSize', '388.47 MB');
+newVersion.set('win32Size', '299.04 MB');
+newVersion.set('win64Size', '312.71 MB');
+newVersion.set('releaseNote', '新增功能\n1. 在“发布”界面增加选项，可选择显示视线中心点。\n2. 在节点上增加“显示进度条”选项，配合停留x秒触发区，能够在视线中心点周围显示一个圆环进度条。\n3. 在“发布”界面增加选项，可以选择不显示作品卡片直接进入作品内容。\n\n修复的Bug：\n1. 修正了多次播放视频时的内存泄漏，无论播放多少音视频现在都不会卡顿了。\n2. 重新实现了“淡入淡出”，大幅提升性能。\n3. 修正了切换全景视频或者图片时播放器的抖动。\n4. 修正了音量调节不能设置为0%的问题。\n5. 修正了连续触发切换会黑屏的问题。\n\n对原有功能进行的修改\n1. 一旦切换全景视频或者图片，屏幕上所有的局部内容都会一起消失。\n2. 局部视频播放完毕后不再保留最后一帧画面。');
 
 newVersion.save().then(function (ver) {
   console.log(ver.id + ' saved!');
